f175b2783d9c534863c9e5900b847b3a09ded14b
Hayk-1997 <abrahamyanhayk01@gmail.com>
2024-06-06 02:20:07 +0400

    Changed navigation links

--- components/layout/Sidebar/index.tsx
+++ components/layout/Sidebar/index.tsx
@@ -1,21 +1,20 @@
 import React from "react";
-import CategoryLink from "@/components/molecules/sidebarNavigations/CategoryLink";
-import { PAGES_ROUTER_PATH_NAMES } from "@/constants/router";
-import MaterialLink from "@/components/molecules/SidebarNavigations/MaterialLink";
+import { ADMIN_ROUTES } from "@/constants/router";
+import Link from "next/link";
 
-const Sidebar = () => {
+const Sidebar = (): React.JSX.Element => {
   return (
     <>
       <aside id="sidebar" className="sidebar">
         <ul className="sidebar-nav" id="sidebar-nav">
-          <li className="nav-item">
-            <a className="nav-link" href={PAGES_ROUTER_PATH_NAMES.dashboard}>
-              <i className="bi bi-grid" />
-              <span>Dashboard</span>
-            </a>
-          </li>
-          <CategoryLink />
-          <MaterialLink />
+          {ADMIN_ROUTES.map((route) => (
+            <li className="nav-item" key={route}>
+              <Link className="nav-link" href={route.path}>
+                <i className="bi bi-menu-button-wide" />
+                <span>{route.name}</span>
+              </Link>
+            </li>
+          ))}
         </ul>
       </aside>
     </>
--- constants/router.ts
+++ constants/router.ts
@@ -5,3 +5,18 @@ export const PAGES_ROUTER_PATH_NAMES: TPAgesRouterPAthNames = {
   categories: "/categories?orderDirection=asc&orderBy=Id",
   materials: "/materials?orderDirection=asc&orderBy=Id",
 };
+
+export const ADMIN_ROUTES = [
+  {
+    name: "Dashboard",
+    path: PAGES_ROUTER_PATH_NAMES.dashboard,
+  },
+  {
+    name: "Categories",
+    path: PAGES_ROUTER_PATH_NAMES.categories,
+  },
+  {
+    name: "Materials",
+    path: PAGES_ROUTER_PATH_NAMES.materials,
+  },
+];
