9450a5b7d6d040857a1dfb714064a9ddce5da011
Hayk-1997 <abrahamyanhayk01@gmail.com>
2024-08-07 00:22:35 +0400

    Code fixing

--- components/molecules/DropDowns/CreateProductDropdown/index.tsx
+++ components/molecules/DropDowns/CreateProductDropdown/index.tsx
@@ -26,7 +26,7 @@ const CreateProductDropdown: React.FC<ICreateProductDropdown> = ({
             href="#"
             onClick={() => handleClick(ProductEnum.STATIC_PRODUCT)}
           >
-            Create Product
+            Create Cabinet
           </a>
         </li>
         <li>
