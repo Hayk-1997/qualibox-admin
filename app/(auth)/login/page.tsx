529e3b605d5b5a13bff538aa6d43aa5a178eb941
Hayk-1997 <abrahamyanhayk01@gmail.com>
2024-08-13 01:29:39 +0400

    Fixed build warnings

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoginForm from "@/components/forms/LoginForm";
+import LoginForm from "@/components/Forms/LoginForm";
 
 const LoginPage: React.FC = (): React.JSX.Element => {
   return (
